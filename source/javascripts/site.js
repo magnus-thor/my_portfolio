b675302dfcaa5d7f6e524f8a1616df9cc52cff7c
magnus-thor <magnus.thor.ar@gmail.com>
2017-09-14 10:29:39 +0200

    clicking sidebar projects displays description in main area

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,17 +2,16 @@
 $(document).ready(function() {
   $('.hide_show').hide();
 
+  $('ul a').click(function() {
+    description = $(this).data('show');
+    $('.description desc').text(description);
+  })
+
+  //
   // $('ul a').click(function() {
+  //   $('p').hide();
+  //   $('.hide_show')
   //   $(this.id).show();
   //   console.log(this.id);
   // })
-
-
-  $('ul a').click(function() {
-    $('p').show();
-    // $('.hide_show')
-
-    // $('desc_' + this.id).show();
-    console.log(this.id);
-  })
 });
