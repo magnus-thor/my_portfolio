9ef9b845ee4f612ff2de3e29a6a37acc078199a4
magnus-thor <magnus.thor.ar@gmail.com>
2017-09-15 13:05:01 +0200

    changed styling on weblink text and sidenav background color. Also added some content to job.yml

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,16 +1,24 @@
 // This is where it all goes :)
 $(document).ready(function() {
-  $('#myimage').hide();
+  $('#logo').hide();
+  $('#website_link').hide();
 
-   $(".button-collapse").sideNav({
+  $(".button-collapse").sideNav({
      menuWidth: 200,
      closeOnClick: true
    });
 
   $('ul a').click(function() {
     text = $(this).data('show');
-    picture = $(this).data('picture');
+    link = $(this).data('link');
+    weblink = $(this).data('weblink');
+    $('#logo').show();
     $('.description desc').text(text);
+    $("#github_link").show().attr("href", link);
+    $("#website_link").show().attr("href", weblink);
+    if (weblink == "none") {
+      $('#website_link').hide();
+    }
     // $("#myimage").show().attr("src", picture)
   });
 });
